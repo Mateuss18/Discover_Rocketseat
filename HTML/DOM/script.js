7607aaab61814039d4bb2bfe7211e0460bc66164
Mateuss18 <mateusdfl18@gmail.com>
2021-09-29 00:05:59 -0300

    Estudando DOM

---
+++ HTML/DOM/script.js
@@ -0,0 +1,18 @@
+/* 
+const element = document.getElementById('title')
+Retorna um element 
+
+const element = document.getElementsByClassName('one')
+Retorna HTMLCollection
+
+const element = document.getElementsByTagName('h1')
+Retorna HTMLCollection
+
+const element = document.querySelector('.one')
+Retorna um element
+
+const element = document.querySelectorAll('h1')
+Retorna Nodelist -podendo usar Foreach-
+
+console.log(element)
+*/
