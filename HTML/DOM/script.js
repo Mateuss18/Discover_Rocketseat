12ab8605c05f945d17d2870aaea72fed6db4ce94
Mateuss18 <mateusdfl18@gmail.com>
2021-10-02 00:58:56 -0300

    estudando os elementos DOM

--- HTML/DOM/script.js
+++ HTML/DOM/script.js
@@ -16,3 +16,36 @@ Retorna Nodelist -podendo usar Foreach-
 
 console.log(element)
 */
+
+//const element = document.querySelector('h1')
+
+//element.textContent = "Seja bem vindo(a)"
+//element.innerText = "Como vai?"
+//element.innerHTML = "Espero que goste<i>!</i>"
+
+// const element = document.querySelector('input')
+
+// element.value = "Finalizado"
+
+// const header = document.querySelector('header')
+// header.setAttribute('id', 'header')
+
+// const headerID = document.querySelector('#header')
+
+// console.log(headerID.getAttribute('id'))
+
+// const element = document.querySelector('body')
+
+// element.style.backgroundColor = "#FF0000"
+// console.log(element.style.backgroundColor)
+
+// const element = document.querySelector('body')
+
+// element.classList.add('active', 'blue')
+// console.log(element.classList)
+
+// element.classList.toggle('active') //ativa ou dasativa 
+
+// const element = document.querySelector('h1')
+
+// console.log(element.parentNode)
